c136c2bed47ed0cc87e9f055f8e4621cd6a94b31
MrFunlife <dgfunlife@gmail.com>
2017-09-25 20:39:56 +0700

    Chapter7

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   get '/signup',  to: 'users#new'
   get '/login',   to: 'static_pages#login'
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
