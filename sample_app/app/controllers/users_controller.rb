5c009209f8e61da9ca3e20783b749c9fd7fb65a7
MrFunlife <dgfunlife@gmail.com>
2017-10-06 16:29:19 +0700

    Finish Chapter09

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,27 +1,28 @@
 class UsersController < ApplicationController
 
   def show
-    @user =  User.find(params[:id])
+    @user = User.find(params[:id])
   end
+
 
   def new
     @user = User.new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+    @user = User.new(user_params)
+    if @user.save
+      #Handle a successful save.
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else render 'new'
     end
   end
 
-    private
+  private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :birth_day, :phone_number, :sex, :address, :password, :password_confirmation)
+  end
 end
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002112319) do
+ActiveRecord::Schema.define(version: 20171003094613) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20171002112319) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "birth_day"
+    t.string   "phone_number"
+    t.integer  "sex"
+    t.string   "address"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
